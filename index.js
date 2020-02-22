211e5ab3b37b4e348f0948bb59856a18176a0d36
Thiago Bernardes <tbernardes@gmail.com>
2020-02-22 13:45:36 +1000

    sync

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const core = require('@actions/core');
 
 try {
-  
+
   const pattern = core.getInput('pattern');
   const string = core.getInput('string');
   const replaceWith = core.getInput('replace')
@@ -9,6 +9,7 @@ try {
 
   const regex = new RegExp(pattern, flags);
 
+  core.setOutput('value', string);
   core.setOutput('result', string.replace(regex, replaceWith));
 } catch (error) {
   core.setFailed(error.message);
